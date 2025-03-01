a2699d458eca3e46cdaf0d2d08d1172fac1e87c7
mohsin-ejaz22 <mohsinejaz734@gmail.com>
2025-03-01 17:20:43 +0500

    added bootstrap in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import { UserContextProvider } from './components/ContextApi/UserContext.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 createRoot(document.getElementById('root')).render(
-  <UserContextProvider>
+
     <BrowserRouter>
         <App />
     </BrowserRouter>,
-  </UserContextProvider>
+
 )
